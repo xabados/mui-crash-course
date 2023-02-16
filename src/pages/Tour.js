0dbf0a1e2669f2997a010d92fa10d264d357bb61
xabados <xabados@aceproject.space>
2023-02-16 13:52:18 -0600

    created skeleton ImageCollage file & added img tag

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,11 +1,19 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 
 const Tour = () => (
   <Container sx={{ width: 900 }}>
     <Typography variant="h3" component="h1" marginTop={3}>
       Explore the World in Vegas
     </Typography>
+    <Box marginTop={3} sx={{ display: "flex" }}>
+      <img
+        src="https://media.timeout.com/images/105124791/750/422/image.jpg"
+        alt="vegas"
+        height={325}
+      />
+    </Box>
   </Container>
 );
 

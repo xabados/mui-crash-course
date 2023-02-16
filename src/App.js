df63d685e63f8c8821dd96f80210f5f46f6180ec
xabados <xabados@aceproject.space>
2023-02-16 13:36:15 -0600

    added Tour file as route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SearchAppBar from "./components/AppBar";
+import Tour from "./pages/Tour";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <SearchAppBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Tour />} />
       </Routes>
     </BrowserRouter>
   );

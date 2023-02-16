6e7f34e64eafa7e5954f24b0684bd9f6e201c1dd
xabados <xabados@aceproject.space>
2023-02-16 09:21:09 -0600

    imported typography & box components

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,5 +1,7 @@
 import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const TourCard = () => {
   return (
@@ -10,6 +12,11 @@ const TourCard = () => {
           alt=""
           className="img"
         />
+        <Box paddingX={1}>
+          <Typography variant="subtitle1" component="h2">
+            Immerse into the falls
+          </Typography>
+        </Box>
       </Paper>
     </Grid>
   );

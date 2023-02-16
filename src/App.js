611c2bd70f0671b39b8213d7767cda4b8596d1d3
xabados <xabados@aceproject.space>
2023-02-16 13:11:20 -0600

    refactored code using provided data

--- src/App.js
+++ src/App.js
@@ -13,21 +13,22 @@ function App() {
       <SearchAppBar />
       <Container sx={{ marginY: 5 }}>
         {cities.map((city) => (
-          <Typography
-            variant="h4"
-            component="h2"
-            marginTop={5}
-            marginBottom={3}
-          >
-            Top {city.name} Tours
-          </Typography>
+          <>
+            <Typography
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour, index) => (
+                <TourCard tour={tour} key={index} />
+              ))}
+            </Grid>
+          </>
         ))}
-        {/* <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid> */}
       </Container>
     </div>
   );

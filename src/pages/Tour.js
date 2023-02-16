09c2abcd1c1aa083ce78b554d0ddca6da516e8e5
xabados <xabados@aceproject.space>
2023-02-16 13:41:23 -0600

    imported Typography & Container components

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,3 +1,12 @@
-const Tour = () => <div>HELLO</div>;
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+const Tour = () => (
+  <Container sx={{ width: 900 }}>
+    <Typography variant="h3" component="h1" marginTop={3}>
+      Explore the World in Vegas
+    </Typography>
+  </Container>
+);
 
 export default Tour;

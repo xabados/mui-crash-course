0cf0bc1c857a17c0e74324232c529815d2d3acec
xabados <xabados@aceproject.space>
2023-02-16 09:11:06 -0600

    added image

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,7 +4,12 @@ import Grid from "@mui/material/Grid";
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>Hello</Paper>
+      <Paper elevation={3}>
+        <img
+          src="https://images.unsplash.com/photo-1559344465-ac6ad8e59943?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+          alt=""
+        />
+      </Paper>
     </Grid>
   );
 };

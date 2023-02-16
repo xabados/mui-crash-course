c149504481c2a01c2207c5e9bc66f09e13de6a36
xabados <xabados@aceproject.space>
2023-02-16 11:58:20 -0600

    mapped the titles to their individual Typography

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import Grid from "@mui/material/Grid";
 
 import TourCard from "./components/TourCard";
 import SearchAppBar from "./components/AppBar";
+import cities from "./data.json";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
       <Container sx={{ marginY: 5 }}>
-        <Grid container spacing={5}>
+        {cities.map((city) => (
+          <Typography
+            variant="h4"
+            component="h2"
+            marginTop={5}
+            marginBottom={3}
+          >
+            Top {city.name} Tours
+          </Typography>
+        ))}
+        {/* <Grid container spacing={5}>
           <TourCard />
           <TourCard />
           <TourCard />
           <TourCard />
-        </Grid>
+        </Grid> */}
       </Container>
     </div>
   );

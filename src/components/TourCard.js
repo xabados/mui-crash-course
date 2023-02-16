f65f1834586c081e045086f36e641027b13446ad
xabados <xabados@aceproject.space>
2023-02-16 09:12:49 -0600

    added image styles

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -8,6 +8,7 @@ const TourCard = () => {
         <img
           src="https://images.unsplash.com/photo-1559344465-ac6ad8e59943?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
           alt=""
+          className="img"
         />
       </Paper>
     </Grid>

c2b392e266767ecc4616bd62fb126c9e0a9194d1
xabados <xabados@aceproject.space>
2023-02-16 13:29:43 -0600

    imported router-dom pkg and wrap Home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

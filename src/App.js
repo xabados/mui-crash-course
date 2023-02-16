fdfb5fcf42dffaf5989f12d14b48603b22d292a5
xabados <xabados@aceproject.space>
2023-02-16 13:27:05 -0600

    moved codes from App file to newly Home file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
 import "./App.css";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-
-import TourCard from "./components/TourCard";
-import SearchAppBar from "./components/AppBar";
-import cities from "./data.json";
-import { Typography } from "@mui/material";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {cities.map((city) => (
-          <>
-            <Typography
-              variant="h4"
-              component="h2"
-              marginTop={5}
-              marginBottom={3}
-            >
-              Top {city.name} Tours
-            </Typography>
-            <Grid container spacing={5}>
-              {city.tours.map((tour, index) => (
-                <TourCard tour={tour} key={index} />
-              ))}
-            </Grid>
-          </>
-        ))}
-      </Container>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
